9e8767408130e732c65d83e2365f2cca89f3f88b
Ktbch <odufotekayode4@gmail.com>
2025-07-16 21:08:27 +0100

    user-controller and servcie construction

---
+++ server/src/controller/userController.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from "express"
+
+// profile creation
+// getOne Profile
+// update Perofile
+// delete user
+
+const createUserProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try
+    {
+
+    } catch (error)
+    {
+
+    }
+}
+const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+    try
+    {
+
+    } catch (error)
+    {
+
+    }
+}
+const getOneProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try
+    {
+
+    } catch (error)
+    {
+
+    }
+}
+const updateUser = async (req: Request, res: Response, next: NextFunction) => {
+    try
+    {
+
+    } catch (error)
+    {
+
+    }
+}

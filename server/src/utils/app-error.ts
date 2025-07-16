27769eae839a04ac5e2a10996670a6ae5aa60c3c
Ktbch <odufotekayode4@gmail.com>
2025-07-16 18:05:03 +0100

    app-error done

---
+++ server/src/utils/app-error.ts
@@ -0,0 +1,46 @@
+export class AppErr extends Error {
+    statusCode: number
+    status: string
+    isOperatoinal: boolean
+
+    constructor (message: string, statusCode: number) {
+
+        super(message)
+
+        this.statusCode = statusCode
+        this.status = `${ statusCode }`.startsWith('4') ? 'fail' : 'error'
+        this.isOperatoinal = true
+    }
+
+}
+
+// notFound
+// UnAuthorised
+// ineternalServerError
+// badRequest
+
+export class NotFoundError extends AppErr {
+    constructor (message = 'Not found') {
+        super(message, 404)
+    }
+}
+
+
+export class BadRequestError extends AppErr {
+    constructor (message = 'Not found') {
+        super(message, 401)
+    }
+}
+
+
+export class UnAuthorisedRequestError extends AppErr {
+    constructor (message = 'Not found') {
+        super(message, 403)
+    }
+}
+
+export class ineternalServerError extends AppErr {
+    constructor (message = 'Not found') {
+        super(message, 401)
+    }
+}

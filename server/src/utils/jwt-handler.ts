0159fc9f535a92c813d77a1b5a574775b3fc9b41
Ktbch <odufotekayode4@gmail.com>
2025-07-16 20:57:27 +0100

    auth_v1_completed

--- server/src/utils/jwt-handler.ts
+++ server/src/utils/jwt-handler.ts
@@ -7,7 +7,7 @@ export const jwtHandler = {
     generateToken: (user: UserPayload) => {
         const { email } = user
         const acessToken = jwt.sign({ email }, config.JWT_SECRET, { expiresIn: '15m' })
-        const refreshToken = jwt.sign({ email }, config.JWT_SECRET, { expiresIn: '7d', algorithm: "ES512" })
+        const refreshToken = jwt.sign({ email }, config.JWT_SECRET, { expiresIn: '7d', algorithm: "HS384" })
         return { refreshToken, acessToken }
     },
     verify: (token: string) => {

e6a019fba842227bb7e6e291cc22e9b8aa6d3d26
Ktbch <odufotekayode4@gmail.com>
2025-07-20 18:07:15 +0100

    comencement

---
+++ server/src/controller/mentorshpController.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express"
+
+
+export const getAllMentors = async (req: Request, res: Response, next: NextFunction) => {
+    try
+    {
+
+    } catch (error)
+    {
+
+    }
+}

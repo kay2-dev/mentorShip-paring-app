6cb8f58d38846fde57b7ffb1f601e86fe853e601
Ktbch <odufotekayode4@gmail.com>
2025-07-20 18:24:13 +0100

    creatiof mentor-ship-repository

---
+++ server/src/repository/mentor-ship-repository.ts
@@ -0,0 +1,16 @@
+import { usersTable } from "../db/schema";
+import { UserRepository } from "./user-repository";
+import { eq } from "drizzle-orm";
+
+
+// i want the uSerRepository to represent the base class of this menbtorship Repository
+class MentorShipRepository extends UserRepository {
+    constructor () {
+        super()
+    }
+
+    async getAllMentors () {
+        return await this.db.select().from(usersTable).where(eq(usersTable.roles, 'mentor'))
+    }
+
+}

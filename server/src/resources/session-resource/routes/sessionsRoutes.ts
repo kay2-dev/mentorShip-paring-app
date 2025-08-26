aa5fb21a99ce2b08183e1cd7ad39579c988093ce
kay2-dev <odufotekayode10@gmail.com>
2025-08-26 16:59:53 +0100

    feat:booking-session:  routing system v1

--- server/src/resources/session-resource/routes/sessionsRoutes.ts
+++ server/src/resources/session-resource/routes/sessionsRoutes.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { API_END_POINTS } from "../../../constant/constants";
 import { createSessionController, getSessionController, updateSessionController } from "../controller/session-controller";
+import { authGuard } from "../../../middleware/auth-Guard";
+import { checkRole } from "../../../middleware/checkRole";
+import { validateIncomingData } from "../../../middleware/validate-incoming-data";
+import { zodBookSessionSchema } from "../../../lib/zod-validations-schema";
 
 
 export const sessionsRouter = Router();
@@ -8,9 +12,9 @@ export const sessionsRouter = Router();
 
 
 
-sessionsRouter.get(API_END_POINTS.sessionsEndPoints.sessions, getSessionController);
-sessionsRouter.post(API_END_POINTS.sessionsEndPoints.getAllSessionsForMentor, createSessionController);
-sessionsRouter.put(API_END_POINTS.sessionsEndPoints.getAllSessionsForMentee, updateSessionController);
-sessionsRouter.get(API_END_POINTS.sessionsEndPoints.sessionsFeedbacks, () => { });
+sessionsRouter.post(API_END_POINTS.sessionsEndPoints.sessions, authGuard, checkRole("mentee"), validateIncomingData(zodBookSessionSchema), createSessionController);
+// sessionsRouter.get(API_END_POINTS.sessionsEndPoints.getAllSessionsForMentor, authGuard, checkRole() createSessionController);
+// sessionsRouter.put(API_END_POINTS.sessionsEndPoints.getAllSessionsForMentee, updateSessionController);
+// sessionsRouter.get(API_END_POINTS.sessionsEndPoints.sessionsFeedbacks, () => { });
 
 
